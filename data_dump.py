d6f08af605b6fb2cae28d076537682a8abb7bc89
Rahul <rahulharshawardhan@gmail.com>
2023-05-28 01:06:45 +0530

    utils files and config files updated

--- data_dump.py
+++ data_dump.py
@@ -6,12 +6,11 @@ import json
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://rahulharshawardhan:uyIAF7VZRnM9y3sn@cluster0.7mr9e03.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient("mongodb+srv://rahulharshawardhan:uyIAF7VZRnM9y3sn@cluster0.7mr9e03.mongodb.net/?retryWrites=true&w=majority")
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
 
-DATA_FILE_PATH = (r"C:\Users\rahul\Engg Wale Bhaiya\Insurance Prediction\Insurance_Prediction\insurance.csv")
+
+DATA_FILE_PATH = (r"G:\Insurance Project\Insurance_Prediction\insurance.csv")
 
 DATABASE_NAME = "INSURANCE"
 COLLECTION_NAME = "INSURANCE_PROJECT"
@@ -25,7 +24,7 @@ if __name__ == "__main__":
     # since our mongodb takes data in key and value pair, we need to transpose our data.
 
     json_record = list(json.loads(df.T.to_json()).values()) # here the data is in key and value pair.
-    # Now we will insert into mongo db.
+    # # Now we will insert into mongo db.
     print(json_record[0])
 
     client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
