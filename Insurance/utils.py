00ef49b230cde72c4459c5b7e836b28854d9a676
Rahul <rahulharshawardhan@gmail.com>
2023-05-28 21:57:33 +0530

    data ingestion

--- Insurance/utils.py
+++ Insurance/utils.py
@@ -6,7 +6,8 @@ from Insurance.exception import InsuranceException
 from Insurance.config import mongo_client
 from Insurance.logger import logging
 
-def get_collection_as_dataframe(database_name:str, collection_name:str)->pd.DataFrame:
+
+def get_collection_as_dataframe(database_name: str, collection_name: str) -> pd.DataFrame:
     try:
         logging.info(f"Reading data from database:{database_name} and collection:{collection_name}")
         df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
